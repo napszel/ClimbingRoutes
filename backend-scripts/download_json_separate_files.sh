63409fca8daaa11d3a942b804a47cbc544cb94fa
Eva Szadeczky-Kardoss <eva@napszel.com>
2019-01-28 18:03:26 +0100

    Echo message in case the json file download fails. So I get an email.

--- backend-scripts/download_json_separate_files.sh
+++ backend-scripts/download_json_separate_files.sh
@@ -4,5 +4,5 @@ set -e
 
 date=$(date +%Y%m%d)
 
-curl -f -g -s "http://www.kletterzentrum.com/routenfinder/finder/Route/?tx_kletroute_kletroutefilter%5Baction%5D=doFilter&tx_kletroute_kletroutefilter%5Bfilter%5D%5Bcategory%5D%5B65%5D=0&type=1390811741&tx_kletroute_kletroutefilter[offset]=0&tx_kletroute_kletroutefilter[limit]=1500" >../daily-saves/routes-$date.json
+curl -f -g -s "http://www.kletterzentrum.com/routenfinder/finder/Route/?tx_kletroute_kletroutefilter%5Baction%5D=doFilter&tx_kletroute_kletroutefilter%5Bfilter%5D%5Bcategory%5D%5B65%5D=0&type=1390811741&tx_kletroute_kletroutefilter[offset]=0&tx_kletroute_kletroutefilter[limit]=1500" >../daily-saves/routes-$date.json || echo Downloading the json failed
 
