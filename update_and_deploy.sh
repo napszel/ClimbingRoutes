0fd1bba5fb2615556ce067bc74b5394dcd19bf81
Eva Szadeczky-Kardoss <eva@napszel.com>
2019-02-25 22:58:35 +0000

    Fixed update_and_deploy.sh to include everyhing needed for daily updates (added to crontab)

--- update_and_deploy.sh
+++ update_and_deploy.sh
@@ -3,10 +3,11 @@
 set -e
 
 cd backend-scripts
-./download_json.sh
+./download_jsons_and_generate_one_new.sh
 python3 update_database.py
 ./update_postcounts.py
 python3 generate_index_html.py
 python3 generate_beta_index_html.py
 python3 generate_routesarray_json.py
+python3 generate_beta_routesarray_json.py
 ./upload_to_atom.sh "$1"
