bb440596b39d077d48cfdd27c8de2d25b8d04908
Eva Szadeczky-Kardoss <eva@napszel.com>
2019-03-17 11:20:16 +0000

    Simplified email notification, just tells which gyms the new routes are from

--- send_email_notification.sh
+++ send_email_notification.sh
@@ -15,16 +15,14 @@ mv $NEW_FILE $OLD_FILE
 cp ./generated/routes.json $NEW_FILE
 diff $NEW_FILE $OLD_FILE > $DIFF || true
 if [ -s $DIFF ]; then
-    NO_OF_NEW=`cat $DIFF | grep '"ID": ' | wc -l`
     #MILANDIA is "" if Greifensee is not found in diff or "Milandia" otherwise
     MILANDIA="" && [[ `cat $DIFF | grep 'OrtBez.*Greifensee'` ]]  && MILANDIA="Milandia"
     GASWERK="" && [[ `cat $DIFF | grep 'OrtBez.*Schlieren'` ]] && GASWERK="Gaswerk"
     PLACE="$GASWERK $MILANDIA"
-    MESSAGE=`cat $DIFF | grep SchwierigkeitsgradBez | cut -f2 -d":"`
     #Save first parameter
     pw=$1
     #Remove first parameter
     shift
     #Send email to the rest of the parameters
-    sendemail -q -f eva@napszel.com -xu printer@risko.hu -xp $pw -t "$@" -u "$NO_OF_NEW new routes at $PLACE" -m "Check it out at $URL \n\n Grades of new routes: \n $MESSAGE" -s mail.atom.hu:587
+    sendemail -q -f climbingroutes@napszel.com -xu printer@risko.hu -xp $pw -t "$@" -u "New routes at $PLACE" -m "Check it out at $URL" -s mail.atom.hu:587
 fi
