7e34e183f36ecf46490d218365174877a7d3b368
Eva Szadeczky-Kardoss <eva@napszel.com>
2019-07-28 18:55:12 +0000

    Fix for profile.html style; Uploading to atom at each update.

--- backend-scripts/upload_to_atom.sh
+++ backend-scripts/upload_to_atom.sh
@@ -3,4 +3,4 @@
 set -e
 
 lftp -c "open sftp://napszel:$1@atom.hu && cd public_html/climbingroutes && put ../index.html && put ../javascript.js && put ../style.css && cd generated && put ../generated/routesarray.js" </dev/null >/dev/null
-lftp -c "open sftp://napszel:$1@atom.hu && cd public_html/climbingroutes && put ../beta.html && put ../javascript-beta.js && put ../beta.css && cd generated && put ../generated/routesarray-beta.js" </dev/null >/dev/null
+lftp -c "open sftp://napszel:$1@atom.hu && cd public_html/climbingroutes && put ../beta.html && put ../profile.html && put ../javascript-beta.js && put ../beta.css && cd generated && put ../generated/routesarray-beta.js" </dev/null >/dev/null
