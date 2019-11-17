3d01f500de179332a63151c9ff3ed0bb1db7c037
Eva Szadeczky-Kardoss <eva@napszel.com>
2019-11-17 17:34:02 +0000

    Fix tenminutely to not update beta

--- tenminutely.sh
+++ tenminutely.sh
@@ -5,6 +5,6 @@ set -e
 cd backend-scripts
 ./update_postcounts.py
 python3 generate_index_html.py
-python3 generate_beta_index_html.py
+#python3 generate_beta_index_html.py
 python3 generate_routesarray_json.py
 ./upload_to_atom.sh "$1"
