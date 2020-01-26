f8975e24b5cce6d41238ca396cd03788cd5fa962
Eva Szadeczky-Kardoss <eva@napszel.com>
2020-01-27 00:09:21 +0100

    move the full_name filling in to the html2json converter

--- backend-scripts/convert_html_to_json.py
+++ backend-scripts/convert_html_to_json.py
@@ -7,10 +7,37 @@ import pprint
 from collections import OrderedDict
 import time
 import json
+from datetime import date
+import sys
 
-#test_html = '../daily-saves/test-format.html'
-milandia_html = '../daily-saves/20200105-mil-routes.html'
-gaswerk_html = '../daily-saves/20200105-gas-routes.html'
+today = date.today()
+formatted_today = today.strftime("%Y%m%d")
+
+milandia_html = '../daily-saves/{}-mil-routes.html'.format(formatted_today)
+gaswerk_html = '../daily-saves/{}-gas-routes.html'.format(formatted_today)
+
+def fill_in_full_names(all_routes):
+  try:
+    full_names = open("nevek.jo", "r")
+  except (FileNotFoundError):
+#    print("nevek.jo file not found in directory. No full_name matching done.")
+    return {}
+    
+  names_list = full_names.readlines()
+  numbers_names = {}
+  for item in names_list:
+    name = item.split(':')[1].strip()
+    number = int(item.split(':')[0])
+    numbers_names[number] = name
+
+  full_name_all_routes = []
+  for route in all_routes:
+    next_route = route.copy()
+    if next_route['number'] in numbers_names:
+      next_route['full_name'] = numbers_names[next_route['number']]
+    full_name_all_routes.append(next_route)
+      
+  return full_name_all_routes
 
 
 def parse_html_to_routes_dict(html, place):
@@ -27,11 +54,12 @@ def parse_html_to_routes_dict(html, place):
     next_route['rid'] = None
     next_route['number'] = int(route.find("a", {"class": 'route_name'}).get('href').split('/')[4])
     next_route['name'] = route.find("a", {"class": 'route_name'}).text
+    next_route['full_name'] = None
     next_route['grade'] = route.find("td", {"class": 'grade'}).text
     next_route['setter'] = route.find("td", {"class": 'route_setter'}).text
     next_route['dat'] = time.strftime('%Y-%m-%d', time.localtime(int(route.find("td", {"class": 'date'}).text)))
     next_route['color'] = None
-    next_route['color-codes'] = route.find("div", {"class": 'iframe-circle'}).get("style")
+    next_route['color_codes'] = route.find("div", {"class": 'iframe-circle'}).get("style")
     next_route['typ'] = 'sport'
     next_route['toprope'] = None
     next_route['toppas'] = None
@@ -54,11 +82,12 @@ def parse_html_to_routes_dict(html, place):
     next_boulder['rid'] = None
     next_boulder['number'] = int(boulder.find("a", {"class": 'boulder_name'}).get('href').split('/')[4])
     next_boulder['name'] = boulder.find("a", {"class": 'boulder_name'}).text
+    next_boulder['full_name'] = None
     next_boulder['grade'] = boulder.find("td", {"class": 'grade'}).text
     next_boulder['setter'] = boulder.find("td", {"class": 'route_setter'}).text
     next_boulder['dat'] = time.strftime('%Y-%m-%d', time.localtime(int(boulder.find("td", {"class": 'date'}).text)))
     next_boulder['color'] = None
-    next_boulder['color-codes'] = boulder.find("div", {"class": 'iframe-circle'}).get("style")
+    next_boulder['color_codes'] = boulder.find("div", {"class": 'iframe-circle'}).get("style")
     next_boulder['typ'] = 'bould'
     next_boulder['toprope'] = 0
     next_boulder['toppas'] = 0
@@ -74,12 +103,14 @@ def parse_html_to_routes_dict(html, place):
   return routes, boulders
 
 def main(args=None):
-    milandia_routes, milandia_boulders = parse_html_to_routes_dict(milandia_html, "mil")
-    gaswerk_routes, gaswerk_boulders = parse_html_to_routes_dict(gaswerk_html, "gas")
+  milandia_routes, milandia_boulders = parse_html_to_routes_dict(milandia_html, "mil")
+  gaswerk_routes, gaswerk_boulders = parse_html_to_routes_dict(gaswerk_html, "gas")
+  
+  all_routes = milandia_boulders + gaswerk_routes + gaswerk_boulders + milandia_routes
 
-    all_routes = milandia_routes + milandia_boulders + gaswerk_routes + gaswerk_boulders
-
-    print(json.dumps(all_routes, indent=4, ensure_ascii=False))
+  full_name_all_routes = fill_in_full_names(all_routes)
+  
+  print(json.dumps(full_name_all_routes, indent=4, ensure_ascii=False))
 
 if __name__ == '__main__':
   df = main()
