6101ac10e31a5a4ea9cb9843301a5bcb555a57b6
Eva Szadeczky-Kardoss <eva@napszel.com>
2020-02-16 20:33:09 +0100

    DB fixed -> running update_postcounts script again

--- update_and_deploy2.sh
+++ update_and_deploy2.sh
@@ -6,7 +6,7 @@ cd backend-scripts
 ./download_htmls.sh
 python3 convert_html_to_json.py > ../generated/json_from_html_with_full_names.json
 python3 update_database_v3.py
-#./update_postcounts.py
+./update_postcounts.py
 python3 generate_index_html.py
 python3 generate_routesarray_json.py
 ./upload_to_atom.sh "$1"
