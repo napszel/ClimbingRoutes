4cd9b1c59acf7335565ebdadf8101d5be90ef0ef
Eva Szadeczky-Kardoss <eva@napszel.com>
2020-06-21 18:24:23 +0200

    Add download_and_update_routes_details.py to daily running update_and_deploy script

--- update_and_deploy2.sh
+++ update_and_deploy2.sh
@@ -7,6 +7,7 @@ cd backend-scripts
 python3 convert_html_to_json.py > ../generated/json_from_html.json
 python3 update_database_v3.py
 ./update_postcounts.py
+python3 download_and_update_routes_details.py
 python3 generate_index_html.py
 python3 generate_routesarray_json.py
 ./upload_to_atom.sh "$1"
